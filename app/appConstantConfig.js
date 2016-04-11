f27f12fa6f5bc5e5dcb367dc7280271cd68e068a
Mathias Bylund <materbyl@gmail.com>
2016-04-11 17:21:54 +0200

    Trying to fix merge on master

--- app/appConstantConfig.js
+++ app/appConstantConfig.js
@@ -2,18 +2,17 @@
 
 angular
   .module('energyArtApp')
-  .constant('authConfig', {
-    disabled: false,
-    clientId: 'energy_art',
-    clientSecret: 'jg09zs8f43hlrgr8thhieiwsf8hhsg89389ssh',
-    redirectUri: 'http://energy-art.se'
-  });
-
-angular
-  .module('energyArtApp')
   .constant('twitterConfig', {
     "client_id": "7d254c4ef598b65",
     "client_secret": "d65d9a63a5f2288a58355aa873aaa78f6e907753"
   });
 
 
+ angular
+ .module('energyArtApp')
+ .constant('authConfig', {
+ disabled: false,
+ clientId: 'energy_art',
+ clientSecret: 'jg09zs8f43hlrgr8thhieiwsf8hhsg89389ssh',
+ redirectUri: 'http://energy-art.se'
+ });
