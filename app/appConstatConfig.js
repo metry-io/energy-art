71f2b7df0e07a1f87146d7c5c13d7452c8f2e1f0
Mathias Bylund <materbyl@gmail.com>
2016-04-11 18:00:26 +0200

    Trying to fix build'

--- app/appConstatConfig.js
+++ app/appConstatConfig.js
@@ -2,6 +2,20 @@
 
 angular
   .module('energyArtApp')
+  .constant('twitterConfig', {
+    "client_id": "7d254c4ef598b65",
+    "client_secret": "d65d9a63a5f2288a58355aa873aaa78f6e907753"
+  });
+
+angular
+  .module('energyArtApp')
+  .constant('authConfig', {
+    privateToken: '08161b27d332aeaa6f922bb795e65b3f2bb6bd840fde3cf1735d03113af6'
+  });
+
+/*
+angular
+  .module('energyArtApp')
     .constant('authConfig', {
        disabled: false,
        clientId: 'energy_art',
@@ -10,3 +24,4 @@ angular
     });
 
 
+*/
